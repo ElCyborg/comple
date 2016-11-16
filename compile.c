25436e2f33b1249f9e51dda743ee30c843f24d1d
Ryan Hoeck <ryan.hoeck.01@gmail.com>
2016-11-15 21:07:39 -0500

    Added parse and advance function, still need to include emit code

--- compile.c
+++ compile.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "lexer.h"
 
 
 #define MAX_SYMBOL_TABLE_SIZE 100
@@ -23,29 +24,10 @@ typedef struct symbol {
 } symbol;
 
 
-// token type
-typedef enum {
-	nulsym = 1, identsym, numbersym, plussym, minussym,
-	multsym, slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
-	gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-	periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-	whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-	readsym , elsesym
-} token_type;
-
-typedef struct{
-	int op; //opcode
-	int l;  //lexicographical level
-	int m;  //modifier
-} code_struct;
-
 // semantic value
 // either string (char *) for the name of a constant/an identifier
 // or int for the value of the number
-typedef union {
-	char *id;
-	int num;
-} LVAL;
+
 
 
 //global var holding current token type.
@@ -53,10 +35,15 @@ token_type token;
 
 // global variable holding the semantic value of the current token
 LVAL lval;
+//lval.id = malloc(sizeof(char) * 12); // ident up to 11 chars
 
 //counts synbols in symbol_table
 int symbol_count;
 symbol symbol_table[MAX_SYMBOL_TABLE_SIZE];
+
+char *rawCode;
+int rawCodeIndex = 0;
+int rawCodeSize = 0;
 
 int cx;
 code_struct code[CODE_SIZE];
@@ -70,7 +57,7 @@ void advance();
 // function get_symbol that looks up a symbol in symbol table by name and
 // returns pointer symbol if found and NULL if not found
 
-symbol *get_symbol(char[] name);
+symbol *get_symbol(char* name);
 
 
 FILE *fin;
@@ -78,7 +65,7 @@ FILE *fout;
 
 // function put_symbol that puts a symbol into symbol table provided that
 // a symbol with this name does not exist (calls error function if name already exists)
-void put_symbol(int kind, char[] name, int num, int level, int modifier);
+void put_symbol(int kind, char* name, int num, int level, int modifier);
 
 void emit(int op, int l, int m);
 void program();
@@ -89,6 +76,9 @@ void expression();
 void term();
 void factor();
 void error(int n);
+char *createSourceNoComment(char *code, int fileLength);
+int readNextToken(char *code, int i, int codeLength);
+char* getNoCommentCode(char* filename);
 
 
 
@@ -96,14 +86,21 @@ void error(int n);
 
 int main(int argc, char **argv){
 
+	int i;
+
 	if (argc < 3) {
 		printf("Please incude file in and out to read\n");
 		exit(1);
 	} else {
-		fin = fopen(argv[1],"r");
-		fout = fopen(argv[2],"w")
-
+		rawCode = getNoCommentCode(argv[1]);
+		fout = fopen(argv[2],"w");
 	}
+
+	while (rawCode[rawCodeSize] != '\0') {
+		rawCodeSize++;
+	}
+
+
 
 	//initalize outcode to 0
 	for(i = 0; i < CODE_SIZE; i++){
@@ -111,6 +108,8 @@ int main(int argc, char **argv){
 		code[i].l = 0;
 		code[i].m = 0;
 	}
+
+
 
 	//start our program call
 	program();
@@ -123,29 +122,35 @@ int main(int argc, char **argv){
 	fclose(fin);
 	fclose(fout);
 
+	return 0;
 
 
-}
-
-symbol *get_symbol(char[] name){
 
 }
 
-void put_symbol(int kind, char[] name, int num, int level, int modifier){
 
-}
+
+//symbol *get_symbol(char[] name){
+
+//}
+
+//void put_symbol(int kind, char[] name, int num, int level, int modifier){
+
+//}
+
+
 
 void advance(){
 
-	if (fscanf(fin,"%s",)) {
-		/* code */
-	}
+	rawCodeIndex = readNextToken(rawCode,rawCodeIndex,rawCodeSize);
+
+
 }
 
 void program(){
 	advance();
 	block();
-	if (token !== periodsym)) {
+	if (token != periodsym) {
 		error(9);
 	}
 }
@@ -165,7 +170,7 @@ void block(){
 			//TODO get symbol from token
 
 			advance();
-			if (token != equalsym) {
+			if (token != eqsym) {
 				error(3);
 			}
 
@@ -242,7 +247,7 @@ void statement(){
 
 		advance();
 		if (token != becomessym) {
-			error(3)
+			error(3);
 		}
 
 		advance();
@@ -318,9 +323,9 @@ void condition(){
 		expression();
 		//TODO get relational op
 
-		if () {
-			error(20);
-		}
+		//if () {
+		//	error(20);
+		//}
 
 		advance();
 		expression();
@@ -497,4 +502,288 @@ void error(int error){
 
 	printf("\n");
 	exit(1);
+}
+
+char* getNoCommentCode(char* filename){
+    FILE *fp = fopen(filename, "r");
+
+    int c;
+    char *code, *real;
+    int i = 0, fileLength;
+
+    code = malloc(20000 * sizeof(char));
+
+    while(fscanf(fp, "%c", &c) != EOF)
+    {
+        code[i] = c;
+        i++;
+        fileLength++;
+    }
+
+    real = createSourceNoComment(code, fileLength);
+
+	return real;
+
+}
+
+int readNextToken(char *rcode, int i, int codeLength)
+{
+
+	if (i >=codeLength) {
+		return -1; //no more code
+	} else {
+
+		for (i; i<codeLength; i++)
+		{
+			while (rcode[i] == ' ') {
+				i++;
+			}
+
+			if(isdigit(rcode[i])){
+				token = numbersym;
+
+
+				if((isdigit(rcode[i+4])) && (isdigit(rcode[i+3])) && (isdigit(rcode[i+2])) && (isdigit(rcode[i+1])))
+				{
+					lval.num = (rcode[i+4] - '0') * 10000;
+					lval.num += (rcode[i+3]- '0') * 1000;
+					lval.num += (rcode[i+2]- '0') * 100;
+					lval.num += (rcode[i+1]- '0') * 10;
+					lval.num += (rcode[i]- '0') * 1;
+					return i=i+5;
+				}
+				else if((isdigit(rcode[i+3])) && (isdigit(rcode[i+2]))&& (isdigit(rcode[i+1])))
+				{
+					lval.num += (rcode[i+3]- '0') * 1000;
+					lval.num += (rcode[i+2]- '0') * 100;
+					lval.num += (rcode[i+1]- '0') * 10;
+					lval.num += (rcode[i]- '0') * 1;
+					return i=i+4;
+				}
+				else if(isdigit(rcode[i+2]) && (isdigit(rcode[i+1])))
+				{
+					lval.num += (rcode[i+2]- '0') * 100;
+					lval.num += (rcode[i+1]- '0') * 10;
+					lval.num += (rcode[i]- '0') * 1;
+					return i=i+3;
+				}
+				else if(isdigit(rcode[i+1]))
+				{
+					lval.num += (rcode[i+1]- '0') * 10;
+					lval.num += (rcode[i]- '0') * 1;
+					return i=i+2;
+				}
+				else{
+					lval.num += (rcode[i]- '0') * 1;
+					return i = i+1;
+				}
+			}
+
+			else if((rcode[i] == 'n') && (rcode[i+1] == 'u') && (rcode[i+2] == 'l') && (rcode[i+3] == 'l')) {
+				token = nulsym;
+				i+=4;
+			}
+
+			else if(rcode[i] == '+'){
+				token = plussym;
+				i+=1;
+			}
+
+			else if(rcode[i] == '-'){
+				token = minussym;
+				i+=1;
+			}
+
+			else if(rcode[i] == '*'){
+				token = multsym;
+				i+=1;
+			}
+
+			else if(rcode[i] == '/'){
+				token = slashsym;
+				i+=1;
+			}
+
+			else if((rcode[i] == 'o') && (rcode[i+2] == 'd') && (rcode[i+3] == 'd')) {
+				token = oddsym;
+				i+=3;
+			}
+
+			else if((rcode[i] == '=')){
+				token = eqsym;
+				i+=1;
+			}
+
+			else if((rcode[i] == '!') && (rcode[i+1] == '=')){
+				token = neqsym;
+				i+=2;
+			}
+
+			else if((rcode[i] == '>') && (rcode[i+1] == '=')){
+				token = geqsym;
+				i+=2;
+			}
+
+			else if((rcode[i] == '<')){
+				token = lessym;
+				i+=1;
+			}
+
+			else if((rcode[i] == '>')){
+				token = gtrsym;
+				i+=1;
+			}
+
+			else if((rcode[i] == '<') && (rcode[i+1] == '=')){
+				token = leqsym;
+				i+=2;
+			}
+
+			else if((rcode[i] == '(')){
+				token = lparentsym;
+				i+=1;
+			}
+
+			else if((rcode[i] == ')')){
+				token = rparentsym;
+				i+=1;
+			}
+
+			else if(rcode[i] == ','){
+				token = commasym;
+				i+=1;
+			}
+
+			else if(rcode[i] == ';'){
+				token = semicolonsym;
+				i+=1;
+			}
+
+			else if(rcode[i] == '.'){
+				token = periodsym;
+				i+=1;
+			}
+
+			else if(rcode[i] == ':' && rcode[i+1] == '='){
+				token = becomessym;
+				i=i+2;
+			}
+
+			else if((rcode[i] == 'b') && (rcode[i+1] == 'e') && (rcode[i+2] == 'g') &&
+			(rcode[i+3] == 'i') && (rcode[i+4] == 'n')){
+				token = beginsym;
+				i=i+5;
+			}
+
+			else if((rcode[i] == 'e') && (rcode[i+1] == 'n') && (rcode[i+2] == 'd' )){
+				token = endsym;
+				i=i+3;
+			}
+
+			else if((rcode[i] == 'i') && (rcode[i+1] == 'f')){
+				token = ifsym;
+				i+=2;
+			}
+
+			else if((rcode[i] == 't') && (rcode[i+1] == 'h') && (rcode[i+2] == 'e') && (rcode[i+3] == 'n')) {
+				token = thensym;
+				i+=4;
+			}
+
+			else if((rcode[i] == 'w') && (rcode[i+1] == 'h') && (rcode[i+2] == 'i') && (rcode[i+3] == 'l') && (rcode[i+4] == 'e')){
+				token = whilesym;
+				i=i+5;
+			}
+
+			else if((rcode[i] == 'd') && (rcode[i+1] == 'o')){
+				token = dosym;
+				i+=2;
+			}
+
+			else if((rcode[i] == 'c') && (rcode[i+1] == 'a') && (rcode[i+2] == 'l') && (rcode[i+3] == 'l')) {
+				token = callsym;
+				i+=4;
+			}
+
+			else if((rcode[i] == 'c') && (rcode[i+1] == 'o') && (rcode[i+2] == 'n') && (rcode[i+3] == 's') && (rcode[i+4] == 't')){
+				token = constsym;
+				i=i+5;
+			}
+
+			else if((rcode[i] == 'v') && (rcode[i+1] == 'a') && (rcode[i+2] == 'r')){
+				token = varsym;
+				return i=i+3;
+			}
+
+			else if((rcode[i] == 'p') && (rcode[i+1] == 'r') && (rcode[i+2] == 'o') && (rcode[i+3] == 'c') && (rcode[i+4] == 'e') && (rcode[i+5] == 'd') && (rcode[i+6] == 'u') &&
+			(rcode[i+7] == 'r') && (rcode[i+8] == 'e')){
+				token = procsym;
+				i+=8;
+			}
+
+			if((rcode[i] == 'r') && (rcode[i+1] == 'e') && (rcode[i+2] == 'a') && (rcode[i+3] == 'd')) {
+				token = readsym;
+				i+=4;
+			}
+
+			if((rcode[i] == 'w') && (rcode[i+1] == 'r') && (rcode[i+2] == 'i') && (rcode[i+3] == 't') && (rcode[i+4] == 'e')){
+				token = writesym;
+				i=i+5;
+			}
+
+			else if((rcode[i] == 'e') && (rcode[i+1] == 'l') && (rcode[i+2] == 's') && (rcode[i+3] == 'e')) {
+				token = elsesym;
+				i+=4;
+			}
+
+			else if(rcode[i] == ':'){
+				token = colonsym;
+				i=i+2;
+			}
+
+			else if(isalpha(rcode[i])){
+				int j;
+
+				token = identsym;
+
+				for (j = i; isalpha(rcode[j]); j++) {
+					lval.id[j - i] = rcode[i];
+				}
+
+				i = i + j;
+
+			}
+		}
+	}
+
+}
+
+char *createSourceNoComment(char *rcode, int fileLength)
+{
+    int i;
+    char *real;
+
+    real = malloc(20000 * sizeof(char));
+
+    //printf("source rcode without comments:\n-----------------------------\n");
+    for(i=0; i < fileLength; i++)
+    {
+        if(rcode[i] == '/' && rcode[i+1] == '*')
+        {
+            //skip ahead 2 spaces.
+            i=i+2;
+            while(rcode[i] != '*' && rcode[i+1] != '/')
+                i++;
+        }
+
+        if(rcode[i] != '*' && rcode[i] != '/')
+        {
+            //printf("%c", rcode[i]);
+            real[i]=rcode[i];
+        }
+    }
+    //printf("\n\ntokens\n-------\n");
+
+    return real;
+
 }
